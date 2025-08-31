d652ab3bd84fda86dfbef2c20209609060d0d1f6
romankhanromii <romankhanrk224@gamil.com>
2025-08-31 08:08:55 +0300

    img url

--- server.js
+++ server.js
@@ -64,7 +64,8 @@ app.use('/uploads', (req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*'); 
   res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
   next();
-}, express.static(path.join(__dirname, 'uploads')));
+}, express.static(path.resolve(process.cwd(), 'uploads')))
+
 
 // API routes
 const apiVersion = process.env.API_VERSION || 'v1';
